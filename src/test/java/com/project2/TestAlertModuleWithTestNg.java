a5f05f10f01bbb3927b5a03f82a0a301122c7d30
Akash Anand <akash10.anand@gmail.com>
2022-06-18 12:17:42 +0530

    added testng xml file and some other configs

---
+++ src/test/java/com/project2/TestAlertModuleWithTestNg.java
@@ -0,0 +1,32 @@
+package com.project2;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestAlertModuleWithTestNg {
+    
+  WebDriver driver = Config.configuration();
+    
+    @Test
+    public void alert()
+    {
+        AlertModule alertmodule=new AlertModule(driver);
+        driver.findElement(alertmodule.dialog).click();
+        driver.findElement(alertmodule.simplealert).click();
+        alertmodule.genricAlert("accept");
+        driver.findElement(alertmodule.confirmalert).click();
+        alertmodule.genricAlert("dismiss");
+        driver.findElement(alertmodule.promptalert).click();
+        alertmodule.genricAlert("name");
+        driver.findElement(alertmodule.modernalert).click();    
+    }
+
+
+
+
+    }
+
+
+
+
